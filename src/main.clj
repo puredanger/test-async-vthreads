a510073f8bfc97597056846d22a73ac3e2b247d6
Alex Miller <alex.miller@cognitect.com>
2025-08-26 16:27:02 -0500

    add parking ops

--- src/main.clj
+++ src/main.clj
@@ -7,9 +7,21 @@
 
 (defn -main [& args]
   (println "running main")
+  (let [c (a/chan 10)]
+    (a/>!! c 1)
+    (println (str "thread "
+      (try
+        (a/<! c)
+        "parking op worked"
+        (catch Throwable _ "parking op throws")) "\n"))
 
-  (a/go (println (str "go " (thread-type) "\n")))
+    (a/>!! c 2)
+    (a/io-thread (println (str "io-thread " (thread-type) " " 
+                               (try (a/<! c) "parking op worked" (catch Throwable _ "parking op throws")) "\n")))
 
-  (a/io-thread (println (str "io-thread " (thread-type) "\n")))
+    (a/>!! c :works)
+    (a/go (println (str "go " (thread-type) " parking op: " (a/<! c) "\n")))
+
+    (a/close! c))
   
   (Thread/sleep 1000))
